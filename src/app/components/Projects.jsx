7f6ed689ffe9db6ec6050fbc77e6fae1b37e04ba
Amir <chelabi.ext@whitebay.limited>
2025-09-08 00:16:52 +0100

    implimented tasks global store

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import { useState } from "react";
+import useTaskStore from "../../store/useTaskStore";
+
 
 const Projects = () => {
     const projects =["Project1", "Project2", "Project3", "Project4", "Project5", "Project6", "Project7", "Project8", "Project9", "Project10"];
     const [selectedCategory, setSelectedCategory] = useState("Ongoing")
+    const {tasks} = useTaskStore()
 
     return (
         <div className="space-y-3">
             <h2 className="text-xl font-semibold">projects</h2>
             <nav className="flex flex-col space-y-2">
                 {projects.map((pro) => (
-                    <Link key={pro} href={`/${pro}/${selectedCategory}`} className="w-full bg-purple-500 hover:bg-purple-400 text-white px-4 py-2 rounded-lg text-left">
-                        {pro}
+                    <Link key={pro} href={`/${pro}`} className="w-full bg-purple-500 hover:bg-purple-400 text-white px-4 py-2 rounded-lg text-left">
+                        {pro} ({tasks[pro]?.length || 0} tasks)
                     </Link>
                 ))}
             </nav>
